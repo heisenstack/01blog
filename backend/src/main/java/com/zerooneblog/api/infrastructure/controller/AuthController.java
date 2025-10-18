49917b7af8db16a9ba9d9734ad3faee2ed898619
heisenstack <dinaniamine@gmail.com>
2025-10-18 22:45:05 +0100

    signup endpoint starter

---
+++ backend/src/main/java/com/zerooneblog/api/infrastructure/controller/AuthController.java
@@ -0,0 +1,16 @@
+    package com.zerooneblog.api.infrastructure.controller;
+
+    import org.springframework.web.bind.annotation.*;
+
+import com.zerooneblog.api.interfaces.dto.UserRegistrationRequest;
+
+    @RestController
+    @RequestMapping("/api/auth")
+    public class AuthController {
+
+        @PostMapping("/signup")
+        public String RegisterUser(@RequestBody UserRegistrationRequest request) {
+            System.out.println("Registering user: " + request.username);
+            return "User Registered: " + request.username;
+        }
+    }
---
+++ backend/src/main/java/com/zerooneblog/api/interfaces/dto/UserRegistrationRequest.java
@@ -0,0 +1,8 @@
+package com.zerooneblog.api.interfaces.dto;
+
+public class UserRegistrationRequest {
+    public String username;
+    private String email;
+    private String password;
+
+}
