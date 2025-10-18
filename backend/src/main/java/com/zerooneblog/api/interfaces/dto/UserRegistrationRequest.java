342d5ce862b268f194b2fa0319070edd61e9a7ea
heisenstack <dinaniamine@gmail.com>
2025-10-18 23:19:17 +0100

    sigup getter & setters

--- backend/src/main/java/com/zerooneblog/api/interfaces/dto/UserRegistrationRequest.java
+++ backend/src/main/java/com/zerooneblog/api/interfaces/dto/UserRegistrationRequest.java
@@ -5,4 +5,13 @@ public class UserRegistrationRequest {
     private String email;
     private String password;
 
+    // Getters
+    public String GetUsername() { return username; }
+    public String GetEmail() { return email; }
+    public String getPassword() { return password; }
+
+    // Setters
+    public void SetUsername(String username) { this.username = username;}
+    public void SetEmail(String email) { this.email = email;}
+    public void SetPassword(String password) { this.password = password;}
 }
