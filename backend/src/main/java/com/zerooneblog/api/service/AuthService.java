23e2fdfcc58c67704920ba200d31f83d9b207171
heisenstack <dinaniamine@gmail.com>
2025-10-19 16:46:56 +0100

    Authenticate user setup

--- backend/src/main/java/com/zerooneblog/api/service/AuthService.java
+++ backend/src/main/java/com/zerooneblog/api/service/AuthService.java
@@ -2,20 +2,36 @@ package com.zerooneblog.api.service;
 
 import com.zerooneblog.api.infrastructure.persistence.UserRepository;
 import com.zerooneblog.api.interfaces.dto.UserRegistrationRequest;
+import com.zerooneblog.api.interfaces.dto.UserLoginRequest;
 import com.zerooneblog.api.domain.User;
+
+
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 
 @Service
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public String authenticateUser(UserLoginRequest userLoginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtTokenProvider.generateToken(authentication);
     }
 
     public User registerUser(UserRegistrationRequest registrationRequest) {
