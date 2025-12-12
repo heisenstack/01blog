da0ef4c47b8b4716ddf8f74942e2b80f4046b05b
heisenstack <dinaniamine@gmail.com>
2025-12-12 23:21:34 +0100

    pageable posts home

--- backend/src/main/java/com/zerooneblog/api/interfaces/dto/PostsResponseDto.java
+++ backend/src/main/java/com/zerooneblog/api/interfaces/dto/PostsResponseDto.java
@@ -9,15 +9,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostsResponseDto {
-    private List<PostResponse> posts;
+    private List<PostResponse> content;
     private int pageNumber;
     private long pageSize;
     private int totalPages;
     private long totalElements;
     private boolean last;
 
-     public PostsResponseDto(List<PostResponse> posts, int pageNumber, int pageSize, long totalElements, int totalPages, boolean last) {
-        this.posts = posts;
+     public PostsResponseDto(List<PostResponse> content, int pageNumber, int pageSize, long totalElements, int totalPages, boolean last) {
+        this.content = content;
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
         this.totalElements = totalElements;
